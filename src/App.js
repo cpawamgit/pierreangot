536f154c6bca7a0f5c1dab8796759dac915a9389
cpawamgit <cyril.morin.tai@gmail.com>
2021-01-14 15:01:18 +0100

    warning pop up added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import files from "./site.json";
 import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf';
 import { pdfjs } from 'react-pdf';
+//import { act } from 'react-dom/test-utils';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+console.log(navigator.userAgent)
+
+function PopUp(props) {
+  return (
+    <div className="popUpDiv">
+      <p>Certains documents sont très lourds et peuvent mettre du temps à charger.</p>
+      <p>Cliquer sur parcourir pour parcourir les partitions</p>
+      <p>Veuillez noter que les documents contiennent des pages blanches pour le tirage</p>
+      <p>Cliquer sur ok pour faire disparaitre ce message</p>
+      <button onClick={props.hidePopUp}>
+        OK !
+        </button>
+    </div>
+  );
+}
 
 function PdfDisplayer(props) {
   const [numPages, setNumPages] = useState(null);
@@ -34,6 +50,7 @@ function PdfDisplayer(props) {
         file={toDisplay}
         onLoadSuccess={onDocumentLoadSuccess}
         style={{ margin: 0, padding: 0 }}
+        error=""
       >
         <Page pageNumber={pageNumber}
           height={800}
@@ -41,6 +58,7 @@ function PdfDisplayer(props) {
           scale={props.scale}
           renderMode="svg"
           rotate={props.rotate}
+          error=""
         />
       </Document>
       <div className="navigationButtons">
@@ -90,6 +108,8 @@ class App extends React.Component {
       displayPrice: false,
       loading: false,
       rotation: 0,
+      popUp: false,
+      blockPopUp: false,
     }
     this.baseButtonHandleClick = this.baseButtonHandleClick.bind(this);
     this.displayListHandleClick = this.displayListHandleClick.bind(this);
@@ -100,6 +120,8 @@ class App extends React.Component {
     this.displayPrice = this.displayPrice.bind(this);
     this.rotate = this.rotate.bind(this);
     this.endLoading = this.endLoading.bind(this);
+    this.returnFunc = this.returnFunc.bind(this);
+    this.hidePopUp = this.hidePopUp.bind(this);
   }
 
   scrollToTop() {
@@ -133,6 +155,28 @@ class App extends React.Component {
     });
   }
 
+  returnFunc() {
+    let actualPath = this.state.fullPath.split('/');
+    actualPath = actualPath.filter(item => item !== "");
+    let newPathDisplay = "/";
+    let obj1 = [...files];
+    if (actualPath.length > 1) {
+      actualPath.pop();
+      actualPath[0] = "sitePdfs/";
+      newPathDisplay += `${actualPath.join('/')}/`;
+      for (let i = 0; i < actualPath.length; i++) {
+        obj1 = obj1.filter(item => item.name === actualPath[i]);
+        obj1 = obj1[0];
+        obj1 = obj1.contents;
+      }
+      this.setState({
+        actualObject: obj1,
+        fullPath: newPathDisplay
+      });
+    }
+
+  }
+
   displayListHandleClick(name, typeofbutton, index) {
     if (typeofbutton === "directory") {
       this.setState({
@@ -145,9 +189,17 @@ class App extends React.Component {
         fileName: `${this.state.fullPath}${name}`,
         loading: true
       });
-      setTimeout(this.endLoading, 2000)
+      setTimeout(this.endLoading, 4000)
     }
     this.scrollToTop();
+  }
+
+  hidePopUp() {
+    this.setState({
+      blockPopUp: true
+    });
+    console.log("hidePopUp");
+    console.log(this.state.hidePopUp);
   }
 
   endLoading() {
@@ -180,8 +232,10 @@ class App extends React.Component {
       displayHome: false,
       displayBrowse: true,
       dislayWiki: false,
-      displayPrice: false
+      displayPrice: false,
+      popUp: true
     });
+    this.scrollToTop();
   }
 
   displayPrice() {
@@ -194,9 +248,14 @@ class App extends React.Component {
   }
 
   render() {
-    const pathStyle = this.state.loading ? { color: "red" } : null;
+    console.log("render blockPopUp");
+    console.log(this.state.blockPopUp);
+    console.log("render PopUp");
+    console.log(this.state.popUp);
+    const pathStyle = this.state.loading ? { color: "red", height: "max-content" } : null;
     const baseButton = <button onClick={this.baseButtonHandleClick}>Parcourir</button>
-    const reinitButton = <button onClick={this.reinitState}>Réinitialiser</button>
+    const reinitButton = <button style={{ width: "100px" }} onClick={this.reinitState}>Réinitialiser</button>
+    const returnButton = <button style={{ width: "100px" }} onClick={this.returnFunc}>Retour</button>
     const displayList = this.state.actualObject.map((item, index) => {
       return (
         <button onClick={() => { this.displayListHandleClick(item.name, item.type, index) }}
@@ -208,12 +267,12 @@ class App extends React.Component {
         </button>
       );
     });
-    //todelete after test
-    displayList.unshift(<button style={pathStyle}>{this.state.fullPath}{this.state.loading && "...Chargement du document"}</button>)
+    displayList.unshift(<button style={pathStyle}>{this.state.fullPath}{this.state.loading && "...Chargement du document"}</button>);
+    displayList.unshift(returnButton)
     const displayDiv = <div className="displayDiv">{reinitButton}{displayList}</div>
     return (
       <div>
-        {<div className="header">
+        <div className="header">
           <h1>Pierre Angot, Compositeur Français</h1>
           <div className="menu">
             <p onClick={this.displayHome}>Acceuil</p>
@@ -221,8 +280,10 @@ class App extends React.Component {
             <a href="https://fr.wikipedia.org/wiki/Pierre_Angot" rel="noreferrer" target="_blank" id="wiki">Wikipedia</a>
             <p onClick={this.displayPrice}>Tarifs et droits d'utilisation</p>
           </div>
-        </div>}
-        {this.state.displayBrowse && <div className="searchAndDisplay">
+        </div>
+        {this.state.displayBrowse && 
+        <div className="searchAndDisplay">
+          {this.state.popUp && !this.state.blockPopUp && <PopUp hidePopUp={this.hidePopUp} />}
           <div className="searchDiv">
             {this.state.baseButton ? baseButton : null}
             {!this.state.baseButton && displayDiv}
@@ -237,11 +298,114 @@ class App extends React.Component {
           />
         </div>}
         {this.state.displayHome &&
-          <div className="homeDiv">
+          <div>
+            <div className="homeDiv"> {/*backgroundDiv*/}
+
+            </div>
+            <div className="homeTextWrapper">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Pierre_%26_Micka%C3%ABl_02.JPG"
+                alt="Pierre Angot"
+                height="222px"
+                width="198"
+              >
+              </img>
+              <h2>"Ajouter et ne rien détruire"</h2>
+              <p>Pierre ANGOT est un compositeur français né en Normandie , le premier mars 1958.
+              Dans sa jeunesse il fût autant musicien de jazz que d'orchestre, il sera aussi professeur de basson.
+</p>
+              <p>
+                Elève en composition d'Alain Abbot il s'en détournera rapidement se mettant en rupture dans les années 90 jusqu’à détruire son travail accompli jusque là : il le jugera trop  conforme à la doxa musicale de la deuxième partie du 20ème siècle jusqu'à aujourd'hui.
+</p>
+              <p>
+                Pour lui, sa production débutera donc réellement en 2002 avec la "Sonatine Picturale", créée cette même année par le pianiste Mickaël Bardin. Il  reniera donc toutes ses pièces antérieures  sauf ce qu'il comptera pour ses 4 premiers Opus qui sont plus de l'ordre de la gageure musicale.
+                Il cherchera à retrouver le contact avec le public perdu dans les courants de l'école de Darmstadt, notamment sériel ou musique concrète.
+                Il ne prêtera pas  plus d'intérêt aux dernières mouvances telles  que le néo tonalisme.
+                On peut donc difficilement qualifier la musique de Pierre ANGOT,  si ce n'est qu'elle est le fruit incessant de longues recherches sur le plan esthétique, recherchant à redonner une vérité émotionnelle à la musique dite savante.
+</p>
+              <p>
+                Son engouement ira à des compositeurs tels qu'Henri Tomasi  ou  Henri Dutilleux pour renouveler le langage musical.
+                La recherche obsessionnelle de nouvelles couleurs musicales, sans jamais se départir de l'héritage du passé, peut qualifier la démarche de Pierre ANGOT :  "Ajouter et ne rien détruire" pourrait être sa devise .
+</p>
+
+            </div>
           </div>
         }
         {this.state.displayPrice &&
-          <div className="priceDiv">
+          <div>
+            <div className="priceDiv">
+              
+          </div>
+          <div className="priceDivWrapper">
+              <h2>Prix pour chaque exécution public </h2>
+              <p>Opus 1</p>
+              <ul>
+                <li key="1">le concert des profs : libre de droit</li>
+                <li key="2">Musique de scène : 75€</li>
+              </ul>
+              <p>Opus 2</p>
+              <ul>
+                <li key="1">Duo : libre de droit</li>
+                <li key="2">Trio : 80€</li>
+              </ul>
+              <p>Opus 3 : 150€</p>
+              <p>Opus 4 : 70€</p>
+              <p>Opus 5 : 120€</p>
+              <p>Opus 6</p>
+              <ul>
+                <li key="1">Ragtime : 20 €</li>
+                <li key="2">Sonatine drolatique : 120€</li>
+              </ul>
+              <p>Opus 7</p>
+              <ul>
+                <li key="1">5 petites pièces suaves : 15 €</li>
+                <li key="2">8 petites pièces : Libre de droit</li>
+                <li key="3">L'infant : Libre de droit</li>
+                <li key="4">Spleen : 80€</li>
+              </ul>
+              <p>Opus 8 : 25€</p>
+              <p>Opus 9</p>
+              <ul>
+                <li key="1">Andante et allegro : 70€</li>
+                <li key="2">Fantaisie romantique : 70€</li>
+                <li key="3">Laconique sonatine : 70€</li>
+                <li key="4">Rapsodie fantasque : 80€</li>
+              </ul>
+              <p>Opus 10 : 180€</p>
+              <p>Opus 11</p>
+              <ul>
+                <li key="1">Badinage : Libre de droit</li>
+                <li key="2">L'aurore : 65€</li>
+              </ul>
+              <p>Opus 12 : 65€</p>
+              <p>Opus 13 : 900€</p>
+              <p>Opus 14 : 180€</p>
+              <p>Opus 15</p>
+              <ul>
+                <li key="1">Improvisation : Libre de droit</li>
+                <li key="2">eregrination 80€</li>
+              </ul>
+              <p>Opus 16 : </p>
+              <ul>
+                <li key="1">version 2000 250€</li>
+                <li key="2">version trio 300€</li>
+                <li key="3">version 2010 250€</li>
+              </ul>
+              <p>Opus 17 : Astarte ou Ishtar : 80€</p>
+              <p>Opus 18 : Brocéliande version 2001 ou 2006 : 280€</p>
+              <p>Opus 19 : </p>
+              <ul>
+                <li key="1">version 2000 : 850€</li>
+                <li key="2">version 2008 : 960€</li>
+              </ul>
+              <p>Opus 20 : </p>
+              <ul>
+                <li key="1">Figure dans la ville trompette piano : 270€</li>
+                <li key="2">Figure dans la ville  piano 4 mains : 280€</li>
+                <li key="3">Ivresse 250€</li>
+              </ul>
+              <h2><a href="mailto: cyril.morin.tai@gmail.com">Contact : cyril.morin.tai@gmail.com</a></h2>
+            </div>
           </div>
         }
 
